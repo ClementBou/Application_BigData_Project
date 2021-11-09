d9cb65eaf473de4fc4fd45dbda30bfad7c9abd37
ealeixoc <enzo.aleixo-carvalho@efrei.net>
2021-11-09 09:53:40 +0100

    Update paths of data_preparation in main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.models_training import models_training
 from src.predict import predict
 
 if __name__ == '__main__':
-    train_data, eval_data = data_preparation('../../dataset/application_train.csv', '../../dataset/application_test.csv')
+    train_data, eval_data = data_preparation('dataset/application_train.csv', 'dataset/application_test.csv')
     train_data, eval_data = feature_engineering(train_data, eval_data)
     model = models_training("Xgboost", train_data)
     results = predict(model, eval_data)
