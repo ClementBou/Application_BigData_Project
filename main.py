640e4a527c4124099a7687d2c722a35d7461ac5a
Clement <clement.boulnager@efrei.net>
2021-11-15 16:03:54 +0100

    Data preparation

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from src.data_preparation import data_preparation
-from src.feature_engineering import feature_engineering
-from src.models_training import models_training
-from src.predict import predict
 
 if __name__ == '__main__':
+
     train_data, eval_data = data_preparation('dataset/application_train.csv', 'dataset/application_test.csv')
-    train_data, eval_data = feature_engineering(train_data, eval_data)
-    model = models_training("Xgboost", train_data)
-    results = predict(model, eval_data)
+
--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,8 +1,30 @@
 import pandas as pd
 
 
+def handling_dataframe(dataframe, stable=False):
+    """
+    return a sample of a cleaned given dataset
+    :param stable: boolean to know if the target need to be stable
+    :param dataframe: a data datset to clear
+    :return: sample of a cleared dataset
+    """
+    # To heavy dataset so we only use a sub dataset
+    if stable:
+        dataframe = dataframe.groupby("TARGET").sample(n=15000, random_state=1).sample(frac=1, random_state=1)
+    else:
+        dataframe = dataframe.sample(n=30000, random_state=1)
+
+    # Fill missed data
+    dataframe.fillna(dataframe.mean(), inplace=True)
+
+    # Drop duplicated data
+    dataframe.drop_duplicates(inplace=True)
+
+    return dataframe
+
+
 def data_preparation(dataset_train, dataset_eval):
-    """Get data set from a csv path.
+    """Get data set from a csv path and clear them.
 
     :param dataset_train: path of training dataset.
     :type dataset_train: str.
@@ -14,6 +36,6 @@ def data_preparation(dataset_train, dataset_eval):
     :rtype: Pandas Dataframe.
     """
 
-    train_data = pd.read_csv(dataset_train)
-    eval_data = pd.read_csv(dataset_eval)
+    train_data = handling_dataframe(pd.read_csv(dataset_train), stable=True)
+    eval_data = handling_dataframe(pd.read_csv(dataset_eval))
     return train_data, eval_data
