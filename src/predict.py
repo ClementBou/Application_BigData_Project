886f67d57db0a7dd8e90819e51cd6bfc91197647
Clement <clement.boulnager@efrei.net>
2021-11-16 12:25:01 +0100

    Upgrade project to MLFlow structure

--- src/predict.py
+++ src/predict.py
@@ -1,31 +1,20 @@
-import pickle
-import xgboost as xgb
+import os
+import mlflow
 
 
-def predict(models_name, eval_data):
+def predict(models_path, eval_data):
     """Ask a model to predict target feature on an evaluating dataset.
 
-    :param models_name: trained model to ask.
-    :type models_name: model.
+    :param models_path: models path.
+    :type models_path: str.
     :param eval_data: evaluating dataset.
     :type eval_data: Pandas Dataframe.
     :return: prediction : model predictions.
     :rtype: prediction: Pandas DataFrame.
     """
-    for model_name in models_name:
-        print("Predictions for : " + model_name)
 
-        if model_name == "Xgboost":
-            model = xgb.XGBClassifier()
-            model.load_model("xgboost.json")
-        elif model_name == "Random Forest":
-            with open('random_forest.pkl', 'rb') as f:
-                model = pickle.load(f)
-        elif model_name == "Gradient Boosting":
-            with open('gradient_boosting.pkl', 'rb') as f:
-                model = pickle.load(f)
-        else:
-            raise ValueError("This model isn't available. Try \"Xgboost\" or \"Random Forest\" or \"Gradient Boosting\"")
-
+    for model_path in os.listdir(models_path):
+        print("Predictions for : " + model_path)
+        model = mlflow.sklearn.load_model(models_path + '/' + model_path)
         predictions = model.predict(eval_data)
         print(predictions)
