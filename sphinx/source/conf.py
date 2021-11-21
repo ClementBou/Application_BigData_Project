1364c1b810c3feb8ac3631b86a465b80cbe5a9be
ealeixoc <enzo.aleixo-carvalho@efrei.net>
2021-11-21 20:17:05 +0100

    Update sphinx config files

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -27,7 +27,7 @@ author = 'E. CARVALHO and C. BOULANGER'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx_autodoc_typehints', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 napoleon_use_rtype = True
 
 # Add any paths that contain templates here, relative to this directory.
